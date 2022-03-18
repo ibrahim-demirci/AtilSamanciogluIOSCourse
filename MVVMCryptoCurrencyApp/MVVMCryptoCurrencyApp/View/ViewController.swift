763d90711d4cc6cc44bd250e1bc9c40c2ff88bec
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-03-19 01:45:57 +0300

    Show datas on tableView

--- MVVMCryptoCurrencyApp/MVVMCryptoCurrencyApp/View/ViewController.swift
+++ MVVMCryptoCurrencyApp/MVVMCryptoCurrencyApp/View/ViewController.swift
@@ -10,36 +10,45 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet var tableView: UITableView!
+    private var cryptoListViewModel: CryptoListViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         
-        let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json"
-)!
+        getData()
+        
+    }
+    
+    func getData() {
+        
+        let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!
         
         CryptoService().downloadCurrencies(url: url) {
             (cryptos) in
             if let cryptos = cryptos {
+                self.cryptoListViewModel = CryptoListViewModel(cryptoCurrencyList: cryptos)
                 
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             }
             
         }
+        
     }
     
     
-    
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return cryptoListViewModel != nil ? cryptoListViewModel.numberOfRowsInSection() : 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoCell", for: indexPath) as! CryptoTableViewCell
-        
-        cell.priceLabel.text = "$30"
-        cell.currencyLabel.text = "AVAX"
+        let cryptoViewModel = self.cryptoListViewModel.cryptoAtIndex(indexPath.row)
+        cell.priceLabel.text = cryptoViewModel.name
+        cell.currencyLabel.text = cryptoViewModel.price
         return cell
         
     }
