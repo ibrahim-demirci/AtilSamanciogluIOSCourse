9ee3f86c9aa16822dd955e4caaf1a3b18b453b73
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-03-18 22:18:29 +0300

    Create Model Class and MVVM Folders

---
+++ MVVMCryptoCurrencyApp/MVVMCryptoCurrencyApp/Model/CryptoCurrency.swift
@@ -0,0 +1,13 @@
+//
+//  CryptoCurrency.swift
+//  MVVMCryptoCurrencyApp
+//
+//  Created by İbrahim Demirci on 18.03.2022.
+//
+
+import Foundation
+
+struct CryptoCurrency: Decodable{
+    let currency: String
+    let price: String
+}
